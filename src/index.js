54d025a59c7ab1659e7adda7504741d4e8b1ec96
Yuan <crecpz@gmail.com>
2022-09-05 20:17:34 +0800

    取消fill-available，新增其他方法

--- src/index.js
+++ src/index.js
@@ -2,11 +2,19 @@
 import { Router } from './routes/Router.js';
 import { updateMode } from "./utils/mode.js";
 
+// 01
+// // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+// let vh = window.innerHeight * 0.01;
+// // Then we set the value in the --vh custom property to the root of the document
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// solution 02
+const appHeight = () => {
+  const doc = document.documentElement
+  doc.style.setProperty("--height", `${window.innerHeight}px`)
+}
+window.addEventListener("resize", appHeight)
+appHeight()
 
 // 監聽 hash 變化 & 加載完畢事件
 window.addEventListener('hashchange', Router)
