297099a0d3f681efc3ebe751f1926e0d8570aca1
Yuan <crecpz@gmail.com>
2022-09-06 17:34:01 +0800

    09/06 測試2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 //Router
-import { Router } from './routes/Router.js';
+import { Router } from "./routes/Router.js";
 import { updateMode } from "./utils/mode.js";
 
 // 01
@@ -10,19 +10,19 @@ import { updateMode } from "./utils/mode.js";
 
 // solution 02
 const appHeight = () => {
-  const doc = document.querySelector(':root');
-  doc.style.setProperty("--height", `${window.innerHeight}px`)
-}
-window.addEventListener("resize", appHeight)
-appHeight()
-
-
+  const doc = document.documentElement;
+  doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+};
+window.addEventListener("resize", appHeight);
+appHeight();
 
 // 監聽 hash 變化 & 加載完畢事件
-window.addEventListener('hashchange', Router)
-window.addEventListener('load', Router)
+window.addEventListener("hashchange", Router);
+window.addEventListener("load", Router);
 
 // 監聽 DOM 載入完畢，並更新光線模式
-window.addEventListener('DOMContentLoaded', updateMode);
+window.addEventListener("DOMContentLoaded", updateMode);
 // 監聽使用者系統光線模式是否被改變
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateMode);
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", updateMode);
