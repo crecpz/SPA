64611cb520d13d8ff7fa33db8437ebb2308fb699
Yuan <crecpz@gmail.com>
2022-09-07 18:44:04 +0800

    取消使用add-item-btn

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,6 +1,26 @@
-const addItemBtn = document.querySelector('#add-item-btn');
+// #input-area 新增監聽
+document.querySelector('#input-area')
+  .addEventListener('click',function(e){
+    const addItemBtn = this.querySelector('#add-item-btn ');
+    const input = document.querySelector('.input-area__input');
+    const submitBtn = document.querySelector('.input-area__submit-btn');
 
-// addItemBtn.addEventListener('click', )
+    addItemBtn.classList.add('input-area__add-btn--active');
+    input.focus();
+  
+
+
+    if(e.target === submitBtn) return false;
+    input.addEventListener('blur', function(e){
+      addItemBtn.classList.remove('input-area__add-btn--active');
+    },{once: true})
+
+  })
+
+
+// 1. 使用 id 與 class 同名，id 給 js 用，class 給 css 有什麼問題? 為什麼不直接用 class 就好?
+// 2. id 在 querySelector 時會比較快嗎?
+
 
 /**
  * 點擊 listOptionBtn 會調用此函數。
