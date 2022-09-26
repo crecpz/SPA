6689c887677f23c7ff93f8e2a8dbf22931c7b80b
Yuan <crecpz@gmail.com>
2022-09-26 16:09:55 +0800

    完成 overview 的「樣式」

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,6 +47,144 @@ export const Home = {
                             </div>
                         </div>
                     </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+
+                    <a href="#" class="overview__link">
+                        <div class="overview__header">工作</div>
+                        <div class="overview__content">
+                            <div class="overview__text overview__text--column">待完成
+                                <span class="overview__number overview__number--lg">15</span>
+                            </div>
+                            <div class="overview__group">
+                                <div class="overview__text">全部
+                                    <span class="overview__number overview__number--sm">35</span>
+                                </div>
+                                <div class="overview__text">已完成
+                                    <span class="overview__number overview__number--sm">08</span>
+                                </div>
+                            </div>
+                            <div class="overview__progress-bar progress">
+                                <span class="progress__value">36%</span>
+                                <div class="progress__outer">
+                                    <div class="progress__inner"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
                 </div>
             </div>
         </div>
